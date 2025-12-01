f078ce19b63485f473787fc6def1945d314c9ec2
jhincita <sadwaifv@gmail.com>
2025-12-01 17:07:39 -0300

    commit de proyecto junto

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Pixzeleria!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
