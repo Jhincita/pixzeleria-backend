c5fc9bf0e384b553bd22655dc06f9c9fa1700c99
jhincita <sadwaifv@gmail.com>
2025-12-05 16:05:08 -0300

    arreglo para conexion

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ app.get("/", (req, res) => {
   res.send("Hello, Pixzeleria!");
 });
 
+// Keep this line to get the port from Railway or default to 8080 locally
 const port = process.env.PORT || 8080;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
