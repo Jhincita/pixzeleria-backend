881923ef61b73fe843e11cbb201c1930015fca47
Mara <m.cantillana@duocuc.cl>
2025-12-10 00:53:32 -0300

    tengo fe

--- src/main/java/com/pixzeleria/pixzeleria/model/order/OrderItem.java
+++ src/main/java/com/pixzeleria/pixzeleria/model/order/OrderItem.java
@@ -1,15 +1,18 @@
 package com.pixzeleria.pixzeleria.model.order;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson. annotation.JsonIgnore;
+import com.fasterxml.jackson. annotation.JsonIgnoreProperties;
 import com.pixzeleria.pixzeleria.model.menu.Product;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok. Setter;
+import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype", discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorValue("OrderItem") 
 public class OrderItem {
 
     @Id
--- src/main/java/com/pixzeleria/pixzeleria/model/order/OrderPizza.java
+++ src/main/java/com/pixzeleria/pixzeleria/model/order/OrderPizza.java
@@ -1,22 +1,21 @@
 package com.pixzeleria.pixzeleria.model.order;
 
 import com.pixzeleria.pixzeleria.model.menu.Pizza;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Getter
 @Setter
 @Entity
+@DiscriminatorValue("OrderPizza")
 public class OrderPizza extends OrderItem {
 
     @ManyToOne(optional = true)
-    private Pizza basePizza; // The menu pizza selected
+    private Pizza basePizza;
 
     @OneToMany(mappedBy = "orderPizza", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderIngredient> ingredients = new ArrayList<>();
