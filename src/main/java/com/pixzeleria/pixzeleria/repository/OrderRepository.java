67e228cece50547cf0daffa3a1a5c366a7245f3e
Mara <m.cantillana@duocuc.cl>
2025-12-10 00:40:00 -0300

    wiwiwi

--- src/main/java/com/pixzeleria/pixzeleria/repository/OrderRepository.java
+++ src/main/java/com/pixzeleria/pixzeleria/repository/OrderRepository.java
@@ -1,11 +1,17 @@
 package com.pixzeleria.pixzeleria.repository;
 
-import com.pixzeleria.pixzeleria.model.order.Order;
+import com. pixzeleria.pixzeleria. model.order.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    
+    @EntityGraph(attributePaths = {"items", "items.product", "client"}) 
+    @Override
+    List<Order> findAll();
+    
     List<Order> findByClient_Username(String username);
 }
